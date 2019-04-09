28547d1d8ca7972fdbecf0af6497db92b2c4fd78
joeveloper <bebosir@gmail.com>
2019-04-09 09:22:54 +0100

    initial commit up to passport JWt auth

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
 //load user model
 const User = require("../../models/User");
@@ -15,18 +17,58 @@ router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 //@access         Public
 
 router.post("/register", (req, res) => {
-  User.findOne({ email: req.body.email }).then(User => {
+  User.findOne({ email: req.body.email }).then(user => {
     //find email that matches request
-    if (User) {
+    if (user) {
       return res.status(400).json({ email: "Email already exists" });
     } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", //size
+        r: "pg", //rating
+        d: "mm" //default
+      });
+
       const newUser = new User({
         name: req.body.name,
         email: req.body.email,
         avatar,
         password: req.body.password
       });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
     }
+  });
+});
+
+//@route          GET api/users/login
+//@description    It logs in new users
+//@access         Public
+
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //Find user by email
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      return res.status(404).json({ email: "user not found" });
+    }
+    bcrypt.compare(password, user.password).then(ismatch => {
+      if (ismatch) {
+        res.json({ email: "Success" });
+      } else {
+        res.status(400).json({ email: "Password Incorrect" });
+      }
+    });
   });
 });
 
